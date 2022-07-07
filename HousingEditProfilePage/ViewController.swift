ed8ac04bd62b89171e13da66ee83de0c770eb4d6
Kamal Rohilla <kamal.rohilla@housing.com>
2022-07-07 12:34:48 +0530

    Initial Commit

---
+++ HousingEditProfilePage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HousingEditProfilePage
+//
+//  Created by Kamal Rohilla  on 07/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
